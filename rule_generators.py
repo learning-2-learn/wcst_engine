f123a3d1debeabacdf18859021cd9b61a53969e6
DetectiveKiwi <jbferre@uw.edu>
2023-05-23 18:17:13 +0000

    Added a human version of the rule generator

--- rule_generators.py
+++ rule_generators.py
@@ -37,3 +37,28 @@ class RandomRuleGenerator:
         self.rule = self.rng.choice(features)
         
         return self.rule
+    
+
+class RandomRuleGeneratorHuman:
+    """
+    An iterable to just generate a random int from 0 - 11 indicating
+    which feature should be the next rule
+
+    It follows statistics from the human version of the task
+    """
+    def __init__(self, seed):
+        self.rng = np.random.default_rng(seed)
+        self.rule = self.rng.integers(0, 12, 1)[0]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self): 
+        """
+        randomly generates integer from 0 - 11
+        """
+        features = np.arange(12)
+        features = features[features!=self.rule]
+        self.rule = self.rng.choice(features)
+        
+        return self.rule
