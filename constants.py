1b2d2537dfce2a71b7223ee8696257f7b7ad5883
Patrick Zhang <pqz317@uw.edu>
2024-08-22 20:12:14 +0000

    ensure engine works with extended feature dimensions

--- constants.py
+++ constants.py
@@ -7,3 +7,12 @@ FEATURE_NAMES = np.array([
 ])
 
 DIM_NAMES = ["Shape", "Color", "Pattern"]
+
+# names for WCST with extended feature dimension
+EXTENEDED_DIM_NAMES = ["Shape", "Color", "Pattern", "Outline"]
+EXTENDED_DIM_FEATURE_NAMES = np.array([
+    'CIRCLE', 'SQUARE', 'STAR', 'TRIANGLE', 
+    'CYAN', 'GREEN', 'MAGENTA', 'YELLOW', 
+    'ESCHER', 'POLKADOT', 'RIPPLE', 'SWIRL',
+    'SOLID', 'DOTTED', 'DASHED', 'DASHDOT'
+])
